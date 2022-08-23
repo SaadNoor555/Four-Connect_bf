a939d1b8cdc37426ba1f91b5686614b389c08287
saad <saad@gmail.com>
2022-08-23 14:20:24 +0600

    drawn the board graphically

--- boardManagement.py
+++ boardManagement.py
@@ -1,6 +1,13 @@
 import numpy as np
+import pygame
 
 W, H = 7, 6
+SQUARE_LEN = 100
+BLUE = (0, 0, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+RADIUS = 0.45*SQUARE_LEN
 
 def makeBoard(w=7, h=6):
     board = [[0 for i in range(w)] for j in range(h)]
@@ -61,3 +68,17 @@ def checkWin(board, col, row, turn):
     if cnt>=4 : return True
 
     return False
+
+def board_graphics_init():
+    pygame.init()
+    
+    B_W = W*SQUARE_LEN
+    B_H = (H+1)*SQUARE_LEN
+    screen = pygame.display.set_mode((B_W, B_H))
+    return screen
+
+def draw_board(board, screen):
+    for col in range(W):
+        for row in range(H):
+            pygame.draw.rect(screen, BLUE, (col*SQUARE_LEN, row*SQUARE_LEN+SQUARE_LEN, SQUARE_LEN, SQUARE_LEN))
+            pygame.draw.circle(screen, BLACK, (int(col*SQUARE_LEN+SQUARE_LEN/2), int(row*SQUARE_LEN+(3*SQUARE_LEN/2))), RADIUS)
--- playGame.py
+++ playGame.py
@@ -1,19 +1,31 @@
 from boardManagement import *
+import sys
+import pygame
 
 GAMEOVER = False
 turn = 1
 
 board = makeBoard()
+# graphics()
+screen = board_graphics_init()
+draw_board(board, screen)
+pygame.display.update()
 while not GAMEOVER:
-    col = int(input("Player "+ str(turn) +" turn place (0-6):"))
-    while(col<0 and col>6):
-        print("invalid input, try again")
-        col = int(input("Player "+ str(turn) +" turn place (0-6):"))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print("down")
+            # col = int(input("Player "+ str(turn) +" turn place (0-6):"))
+            # while(col<0 and col>6):
+            #     print("invalid input, try again")
+            #     col = int(input("Player "+ str(turn) +" turn place (0-6):"))
 
-    row = dropPiece(board, col, turn)
-    print(board)
-    if checkWin(board, col, row, turn)==True: 
-        print("player "+str(turn)+" has won")
-        GAMEOVER = True
-    if turn==1: turn=2
-    else: turn=1
+            # row = dropPiece(board, col, turn)
+            # print(board)
+            # if checkWin(board, col, row, turn)==True: 
+            #     print("player "+str(turn)+" has won")
+            #     GAMEOVER = True
+            # if turn==1: turn=2
+            # else: turn=1
