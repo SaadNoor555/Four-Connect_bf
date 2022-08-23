96d8a65e48a0f1ea138f1762bcddfffde94fd355
nahid <sajidnahid@gmail.com>
2022-08-23 12:20:00 +0600

    check valid loc, place piece, get next valid row in a column

--- boardman.py
+++ boardman.py
@@ -5,6 +5,10 @@ def generate_board(row,col):
 	board = np.zeros((row,col))
 	return board
 
+def generate_filled_cols(col):
+	filled_cols = np.zeros(col)
+	return filled_cols
 
 def show_board(board):
 	print(board)
+
--- initgame.py
+++ initgame.py
@@ -14,5 +14,27 @@ AI_piece = 2
 
 
 gameboard = boardman.generate_board(ROW_no, COLUMN_no)
+filled_cols = boardman.generate_filled_cols(COLUMN_no)
+
+def check_valid_location(board, col):  #check if the selected column is full or not
+	return filled_cols== 0
+
+def place_piece(board, row, col, piece):
+    board[row][col] = piece
+    filled_cols[col] = row
+
+def get_next_open_row(board, col):
+	return int(filled_cols[col]+1) 
+
+place_piece(gameboard, 0, 4, 1)
+place_piece(gameboard, 1, 4, 1)
+place_piece(gameboard, 2, 4, 1)
+
+print(check_valid_location(gameboard, 4))
+
+print(get_next_open_row(gameboard,4))
 
 boardman.show_board(gameboard)
+
+print(filled_cols)
+
