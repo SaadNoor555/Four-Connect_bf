8d1badf3de0869c2672122860b27ab98715e66b2
saad <saad@gmail.com>
2022-08-23 12:51:40 +0600

    winning mechanism added

--- boardManagement.py
+++ boardManagement.py
@@ -1,19 +1,63 @@
 import numpy as np
 
+W, H = 7, 6
+
 def makeBoard(w=7, h=6):
     board = [[0 for i in range(w)] for j in range(h)]
+    W, H = w, h
     return np.array(board)
 
 def dropPiece(board, col, turn):
     if isValidMove(board, col):
-        board[getRow(board, col)][col] = turn
+        row = getEmptyRow(board, col)
+        board[row][col] = turn
+        return row
 
-def getRow(board, col):
-    for row in range(len(board)-1, -1, -1):
+def getEmptyRow(board, col):
+    for row in range(H-1, -1, -1):
         if board[row][col]==0:
             return row
 
 def isValidMove(board, col):
     return board[0][col] == 0
 
-makeBoard()
+def checkWin(board, col, row, turn):
+    # checking horizontally
+    cnt=0
+    for i in range(W):
+        if board[row][i]==turn: cnt+=1
+        else: cnt=0
+        if cnt>=4: return True   
+    
+    # checking vertically
+    cnt =0
+    for i in range(H):
+        if board[i][col]==turn: cnt+=1
+        else: cnt=0
+        if cnt>=4: return True
+
+    # checking diagonal \
+    cnt=1
+    for i in range(1, W):
+        if row+i>=H or col+i>=W : break
+        if board[row+i][col+i]==turn: cnt+=1
+        else: break
+    for i in range(1, W):
+        if row-i<0 or col-i<0 : break
+        if board[row-i][col-i]==turn: cnt+=1
+        else: break
+    if cnt>=4 : return True
+
+    # checking diagonal /
+    cnt=1
+    for i in range(1, W):
+        if row+i>=H or col-i<0 : break
+        if board[row+i][col-i]==turn: cnt+=1
+        else : break
+    for i in range(1, W):
+        if row-i<0 or col+1>=H : break
+        if board[row-i][col+1]==turn : cnt+=1
+        else : break
+    if cnt>=4 : return True
+
+    return False
--- playGame.py
+++ playGame.py
@@ -4,14 +4,16 @@ GAMEOVER = False
 turn = 1
 
 board = makeBoard()
-
 while not GAMEOVER:
     col = int(input("Player "+ str(turn) +" turn place (0-6):"))
     while(col<0 and col>6):
         print("invalid input, try again")
         col = int(input("Player "+ str(turn) +" turn place (0-6):"))
 
-    dropPiece(board, col, turn)
+    row = dropPiece(board, col, turn)
     print(board)
+    if checkWin(board, col, row, turn)==True: 
+        print("player "+str(turn)+" has won")
+        GAMEOVER = True
     if turn==1: turn=2
     else: turn=1
